8535e7f82fa0d09de40d6d6bda58b434c25f55ef
ondrabimka <ondrabimka@seznam.cz>
2024-12-12 22:53:40 +0100

    updated rnn ae

--- financial_kg_ds/models/RNN_autoencoder.py
+++ financial_kg_ds/models/RNN_autoencoder.py
@@ -22,13 +22,15 @@ class RNNDecoder(nn.Module):
     def __init__(self, hidden_dim, output_dim, num_layers, dropout=0.5):
         super(RNNDecoder, self).__init__()
         self.hidden_size = hidden_dim
+        self.output_dim = output_dim
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(hidden_dim, hidden_dim, num_layers, batch_first=True, dropout=dropout)
-        self.fc = nn.Linear(hidden_dim, output_dim)  # Linear layer to map to output_dim
+        self.lstm = nn.LSTM(hidden_dim, output_dim, num_layers, batch_first=True, dropout=dropout)
 
-    def forward(self, x, hidden, cell):
-        out, (hidden, cell) = self.lstm(x, (hidden, cell))
-        out = self.fc(out)  # Project back to output dimension
+    def forward(self, x):
+
+        h0 = torch.zeros(self.num_layers, x.size(0), self.output_dim, device=x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.output_dim, device=x.device)
+        out, (_, _) = self.lstm(x, (h0, c0))
         return out
 
 
@@ -40,10 +42,10 @@ class LSTMAutoencoder(nn.Module):
 
     def forward(self, x):
         # Encode
-        encoder_out, hidden, cell = self.encoder(x)
-
+        encoder_out, _, _ = self.encoder(x)
+                                         
         # Use encoder's output as decoder's input for the initial time step
-        out = self.decoder(encoder_out, hidden, cell)
+        out = self.decoder(encoder_out)
         return out
 
     def get_embedding(self, x):
@@ -60,6 +62,6 @@ if __name__ == "__main__":
     seq_len = 5
     batch_size = 2
 
-    model = LSTMAutoencoder(input_size, hidden_size, num_layers)
+    model = LSTMAutoencoder(input_size, hidden_size, num_layers, dropout=0.2)
     input = torch.randn(batch_size, seq_len, input_size)
     decoded = model(input)
