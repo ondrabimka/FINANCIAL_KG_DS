035432cf5233e350dd405b39dffbb19d91bbe156
ondrabimka <ondrabimka@seznam.cz>
2025-01-02 21:33:37 +0100

    added plot_ticker_graph

--- financial_kg_ds/utils/visualisation.py
+++ financial_kg_ds/utils/visualisation.py
@@ -3,6 +3,38 @@ import networkx as nx
 from torch_geometric.data import Data
 from torch_geometric.utils import to_networkx
 
+def plot_ticker_graph(G: nx.DiGraph, ticker: str):
+
+    """
+    Plot the graph of a ticker and its neighbors
+    
+    Parameters
+    ----------
+    G : nx.Graph
+        The graph to plot
+        
+    ticker : str
+        The ticker to plot
+        
+    Raises
+    ------
+    ValueError
+        If the ticker is not found in the dataset
+    
+    Examples
+    --------
+    >>> plot_ticker_graph(G, 'AAPL')
+    """
+    try:
+        node_index = [node_tuple[0] for node_tuple in list(G.nodes.data("name")) if node_tuple[1] == ticker][0]
+    except IndexError:
+        raise ValueError(f"Ticker {ticker} not found in the dataset")
+    
+    plt.figure(figsize=(10, 10))
+    subgraph = G.subgraph([node_index] + list(G.neighbors(node_index)))
+    nx.draw_networkx(subgraph, pos=nx.spring_layout(subgraph, seed=7), labels = nx.get_node_attributes(subgraph, 'name'),with_labels=True, cmap="Set2")
+    plt.show()
+
 
 def visualize_graph(data: Data, seed: int = 7):
 
